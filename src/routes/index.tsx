9adc9034ae994989b3361be6bc2f79362d9443bf
frsswq <farissaifuddin.work@gmail.com>
2025-12-22 13:19:06 +0700

    warn: force push to replace repo

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,7 @@
-import { Navbar } from '@/components/navbar'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
-  return (
-    <>
-      <Navbar />
-    </>
-  )
+  return <></>
 }
